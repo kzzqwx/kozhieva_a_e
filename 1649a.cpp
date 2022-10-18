cbba0a356bffc38609630514ad260598f12b85c7
amina <113929089+kzzqwx@users.noreply.github.com>
2022-10-18 15:08:47 +0300

    Update 1649a.cpp

--- 1649a.cpp
+++ 1649a.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <vector>
+
 int main(){
-    int n(0);
-    std::cin >> n;
-    int tek(0);
-    std::vector<int> a(n);
-    for (int i(1); i <= n; ++i){
-        std::cin >> tek;
-        a[tek] = i;
-    }
-    for (int i(1); i <= n; ++i){
-        std::cout << a[i] << " ";
+    int t(0);
+    std::cin >> t;
+    while (t--){
+        int n(0);
+        std::cin >> n;
+        std::vector<int> a(n);
+        for (int i(0); i < n; ++i){
+            int tek(0);
+            std::cin >> tek;
+            a[i] = tek;
+        }
+        int ind(0);
+        for (int j(0); j < n; j++){
+            if (a[j] != 0){
+                ind = j;
+            } else {
+                break;
+            }
+        }
+        int ind2(0);
+        for (int z(n - 1); z > 0; --z){
+            if (a[z] != 0){
+                ind2 = z;
+            } else {
+                break;
+            }
+        }
+        if (ind2 - ind >= 0){
+            std::cout << ind2 - ind << '\n';
+        } else {
+            std::cout << 0 << '\n';
+        }
     }
 }
