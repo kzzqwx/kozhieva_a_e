56f6dc4b4d4253f2088be6750f6faf8be2f19eb2
amina <113929089+kzzqwx@users.noreply.github.com>
2022-12-20 13:09:55 +0300

    Update cat-n-mouse.cpp

--- cat-n-mouse.cpp
+++ cat-n-mouse.cpp
@@ -5,9 +5,9 @@
 const double eps = std::numeric_limits<double>::epsilon();
 
 int main() {
-   // std::ofstream f;
+   std::ofstream f;
 
-    //f.open("/Users/amina/CLionProjects/geometry/tests.txt");
+    f.open("/Users/amina/CLionProjects/geometry/tests.txt");
 
     Rdec2D r_m = {0, -20}; // mouse's location
     Rdec2D r_c = {0, 0}; // cat's location
@@ -30,8 +30,8 @@ int main() {
     v_m = -r_m / Norm(r_m) * m_v0_m * time + a_m * time;
 
     double prev = Norm(r_m - r_c);
-    // f << std::fixed << std::setprecision(4)  << 0 << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
-    //  Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c)<< std::endl;
+    f << std::fixed << std::setprecision(4)  << 0 << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
+     Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c)<< std::endl;
     for (int i = 1; i != 0; i++) {
 
         v_c = (r_m - r_c) / Norm(r_m - r_c) * m_v0_c;
@@ -42,33 +42,33 @@ int main() {
         r_c = r_c + v_c * time;
         r_m = r_m + v_m * time;
 
-        // f << std::fixed << std::setprecision(4)  << i << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
-          // Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c)<< std::endl;
+        f << std::fixed << std::setprecision(4)  << i << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
+          Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c)<< std::endl;
 
         // mouse run up to hole(0,0) so if y >= 0 that means it is in the hole
         if (r_m.y >= 0) {
-            // f << std::fixed << std::setprecision(4) << i + 1 << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
-             // Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c) << "\n" << "mouse is alive!!!" << std::endl;
+             f << std::fixed << std::setprecision(4) << i + 1 << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
+             Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c) << "\n" << "mouse is alive!!!" << std::endl;
             break;
         }
         // if distance between cat and mouse is less than r_defeat - mouse is dead
         if (Norm(r_m - r_c) <= r_defeat) {
-            // f << std::fixed << std::setprecision(4) << i + 1 << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
-              // Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c) <<"\n " << "mouse is dead(((" << std::endl;
+             f << std::fixed << std::setprecision(4) << i + 1 << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
+               Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c) <<"\n " << "mouse is dead(((" << std::endl;
             break;
         }
         // if distance on the previous period of time is longer than current distance - mouse has super speed and is in the hole for sure
         // or cat run through the hole
         if (r_c.x < 0 || Norm(r_m - r_c) > pr_d) {
-            // f << std::fixed << std::setprecision(4) << i + 1 << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
-            //  Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c) << "\n" << "mouse is far away" <<std::endl;
+            f << std::fixed << std::setprecision(4) << i + 1 << " " << r_m.x << " " << r_m.y << " " << r_c.x << " " << r_c.y << " " <<
+             Norm(v_m) << " " << Norm(a_m) << " " << Norm(r_m - r_c) << "\n" << "mouse is far away" <<std::endl;
             break;
         }
         // saving current value
         pr_d = Norm(r_m - r_c);
 
     }
-    //f.close();
+    f.close();
 
 
 }
